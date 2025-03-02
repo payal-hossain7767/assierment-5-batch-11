ba273cced96ad5e23b2eeba757ad0cd70f3557ea
payal hossain <payalhossain508@gmail.com>
2025-03-02 11:48:43 +0600

    all section commit

--- js/script.js
+++ js/script.js
@@ -6,9 +6,7 @@ for (const btn of btnClassSeleat) {
     btn.addEventListener('click', (e) => {
         alert('Board updates successfully')
         e.target.setAttribute('disabled', false)
-
         //    time zone
-
         const addToTimeLine = e.target.parentNode.parentNode.childNodes[3].innerText;
         const d = new Date();
         let hour = d.getHours();
@@ -17,22 +15,19 @@ for (const btn of btnClassSeleat) {
         let time = hour + ':' + min + ":" + sec
         const lootTime = document.getElementById("time-to-meet").innerHTML = time;
         // appanet child
-        const cleartimeline = e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[5].childNodes[13].childNodes[1].innerText;
-
-        const seleatContaineclear = document.getElementById('clear');
+        const seleatContainerSet = document.getElementById('clear');
         const div = document.createElement('div');
-
         const p = document.createElement('p')
         p.innerHTML = `<h1 class="mt-2 bg-[#F4F7FF] p-2 rounded-md">You have Complete ${addToTimeLine} at Time ${lootTime}</h1>`
         div.appendChild(p);
-        seleatContaineclear.appendChild(div)
-        
+        seleatContainerSet.appendChild(div)
+        // ----
         count = count - 1;
         const firsttime = setinnerText('task-assigned', count);
-
+        // ----
         number = number + 1;
         const chackBorad = setinnerText('score-count', number)
-        if(count <= 0){
+        if (count <= 0) {
             alert('congreates!! you have completed all the current task');
         }
 
